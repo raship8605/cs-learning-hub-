704d4284660b028bf62ede6b238947f740a93e9e
rashi <rashipardeshi870@gmail.com>
2026-04-30 16:09:17 +0530

    Setting modification

--- cs_hub/settings.py
+++ cs_hub/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_app',  # ✅ ADDED - Your app
+    'learning_app',  
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cs_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],  # ✅ CHANGED - Added templates directory
+        'DIRS': ['templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
